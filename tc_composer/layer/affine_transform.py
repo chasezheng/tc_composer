8e4f2d2f5ffdf25ea01b776fc75111e53ee114f5
Chase Zheng <mzheng086@gmail.com>
2018-06-23 23:00:03 -0700

    tmp

--- tc_composer/layer/affine_transform.py
+++ tc_composer/layer/affine_transform.py
@@ -18,8 +18,7 @@ class AffineTransform(Layer):
     def lang(self) -> str:
         return (f"def {self.id}(float(batch_size, in_n) input, float(out_n, in_n) weight, float(out_n) bias) -> (output) {'{'}\n"
                 "    output(b, n) +=! input(b, i) * weight(n, i)\n"
-                "    output(b, n) = output(b, n) + bias(n)\n" if self.use_bias else ''
-                "}")
+                "    output(b, n) = output(b, n) + bias(n)" if self.use_bias else '') + '}'
 
     def forward(self, input: Variable, outputs: Sequence[Variable] = None, **kwargs):
         return self.tc_unit(input.view(-1, self.in_n), self.weight, self.bias, outputs=outputs, **kwargs)
