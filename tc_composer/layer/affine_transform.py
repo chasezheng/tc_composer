7ef7d630d8b41cf8d718c4ff565a9fc31531f044
Chase Zheng <mzheng086@gmail.com>
2018-06-23 22:54:33 -0700

    tmp

--- tc_composer/layer/affine_transform.py
+++ tc_composer/layer/affine_transform.py
@@ -7,7 +7,8 @@ from .layer import Layer
 
 class AffineTransform(Layer):
     def __init__(self, in_n: int, out_n: int, bias: bool = True, name: str = None):
-        super(AffineTransform, self).__init__(in_n=in_n, out_n=out_n, name=name)
+        super(AffineTransform, self).__init__(name=name)
+        self.in_n = in_n
         self.use_bias = bias
 
         self.weight = Parameter(torch.normal(torch.zeros(out_n, in_n), .1))
@@ -17,7 +18,7 @@ class AffineTransform(Layer):
     def lang(self) -> str:
         return (f"def {self.id}(float(batch_size, in_n) input, float(out_n, in_n) weight, float(out_n) bias) -> (output) {'{'}\n"
                 "    output(b, n) +=! input(b, i) * weight(n, i)\n"
-                "    output(b, n) = output(b, n) + bias(n)\n" if self.use_bias else ''  # todo Can replace with this? `O1(b, n) += B1(n)\n`
+                "    output(b, n) = output(b, n) + bias(n)\n" if self.use_bias else ''
                 "}")
 
     def forward(self, input: Variable, outputs: Sequence[Variable] = None, **kwargs):
